a5ca7eda143ebcf5f2c79019b2dea4466eb0b3d5
b24fm1032 <emvjn44@gmail.com>
2026-04-26 02:05:28 -0500

    Update Rogue.java

--- assignments/classes/Rogue.java
+++ assignments/classes/Rogue.java
@@ -3,17 +3,24 @@
 // ─────── 🔴 Bonus (10 оноо) ───────
 // Rogue класс FINAL байх ёстой. Жишээ: "public final class Rogue ..."
 
-public class Rogue extends Character {
+public final class Rogue extends Character {
 
-    // TODO: Constructor
-    // public Rogue(String name) {
-    //     super(name);             // <-- эхний мөр заавал байх
-    //     this.hp = 100;
-    //     this.maxHp = 100;
-    //     this.mp = 40;
-    //     this.maxMp = 40;
-    // }
+ 
+    public Rogue(String name) {
+       
+        super(name);
+        
+        this.hp = 100;
+        this.maxHp = 100;
+        this.mp = 40;
+        this.maxMp = 40;
+    }
 
-    // TODO: sneakAttack(Character target) → void
-    // - target.takeDamage(50);  // критикал цохилт
+    
+    public void sneakAttack(Character target) {
+        if (target != null) {
+            target.takeDamage(50);
+            System.out.println(this.name + " performed a Sneak Attack on " + target.getName() + "!");
+        }
+    }
 }
